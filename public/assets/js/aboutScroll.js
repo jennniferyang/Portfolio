550e0f193b971aa4bf97aaa765b1ff9dc8074fa6
DopplerKuo <doppler.kuo@gmail.com>
2017-01-13 11:42:35 +0800

    放上文字

--- public/assets/js/aboutScroll.js
+++ public/assets/js/aboutScroll.js
@@ -1,14 +1,14 @@
 $(function(){
-	$(window).bind('scroll resize', function(){
-　　var $this = $(this);
-　　var $this_Top = $this.scrollTop();
-   var $barFixedHeight = $('.sec1').height() - 140;
-　　if($this_Top < $barFixedHeight){
-　　　 $('nav').removeClass('nav-small');
-　　　}
-　　else if($this_Top > $barFixedHeight){
-　　　 $('nav').addClass('nav-small')
-			$('#index .sec2').addClass('animate')
-　　　}
-　　}).scroll();
+
+	var go;
+  $(".nav-list a").click(function(e) {
+  	 e.preventDefault()
+  	 var t = $(this).attr('id')
+     go = $("[data-id='" + t + "']").offset().top
+     $(document.body).animate({
+       scrollTop: (go - 50)
+     })
+  });
+
 })
+

a8ad25c60e772a8e1990c5258004995c14bb90d9
DopplerKuo <doppler.kuo@gmail.com>
2017-01-13 15:18:34 +0800

    更新內文

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -3,6 +3,20 @@ $(window).load(function() {
 		$(this).toggleClass('active');
 		$('.nav-list').toggleClass('active');
 	});
+
+ $('.more-btn').click(function(){
+ 	$(".pop").show()
+ 	var a = $(this).attr('data-pop');
+ 	console.log(a)
+ 	$(".pop-content").hide()
+ 	$(".pop-content[data-pop='" + a + "']").show()
+ 	$('body').css('overflow-y', 'hidden')
+ })
+ $('.bg').click(function(){
+ 	$(".pop").hide()
+ 	$(".pop-content").hide()
+ 	$('body').css('overflow-y', 'auto')
+ })
 })
 
 

83737998681ff62111a31ef67c66884a8c45e63a
DopplerKuo <doppler.kuo@gmail.com>
2018-02-15 03:06:08 +0800

    Fix font-family bug and nav link bug

--- public/assets/js/aboutScroll.js
+++ public/assets/js/aboutScroll.js
@@ -1,14 +1,3 @@
 $(function(){
-
-	var go;
-  $(".nav-list a").click(function(e) {
-  	 e.preventDefault()
-  	 var t = $(this).attr('id')
-     go = $("[data-id='" + t + "']").offset().top
-     $(document.body).animate({
-       scrollTop: (go - 50)
-     })
-  });
-
 })
 

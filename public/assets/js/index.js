c4a76b28c8bbadc509074fa5e5250549149d42e9
DopplerKuo <doppler.kuo@gmail.com>
2018-02-28 00:28:39 +0800

    Add mobile menu btn and adjust timing of event binding

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,40 +1,32 @@
 $(document).ready(function() {
 	$('h1, .header p, .header-icon a, .header .content').addClass('go');
-
-	window.sr = ScrollReveal();
-	sr.reveal('.animation_show', { 
-		duration: 400,
-		delay: 300,
-		afterReveal: function() {
-			// $('.animation_show').animateNumbers(,)
-		}
-	});
-})
-
-$(window).load(function() {
 	
 	$(".menu-btn").click(function() {
 		$(this).toggleClass('active');
 		$('.nav-list').toggleClass('active');
 	});
+	
+	$('.work').click(function(){
+		 $(".pop").show()
+		 var a = $(this).attr('data-pop');
+		 $(".pop-content").hide()
+		 $(".pop-content[data-pop='" + a + "']").show()
+		 $('body').css('overflow-y', 'hidden')
+	})
+	$('.bg').click(function(){
+		 $(".pop").hide()
+		 $(".pop-content").hide()
+		$('body').css('overflow-y', 'auto')
+	})
+	
+})
 
- $('.work').click(function(){
- 	$(".pop").show()
- 	var a = $(this).attr('data-pop');
- 	$(".pop-content").hide()
- 	$(".pop-content[data-pop='" + a + "']").show()
- 	$('body').css('overflow-y', 'hidden')
- })
- $('.bg').click(function(){
- 	$(".pop").hide()
- 	$(".pop-content").hide()
- 	$('body').css('overflow-y', 'auto')
- })
+$(window).load(function() {
 
- $('.pop-content img').each(function() {
- 	var g = $(this).attr("data-src")
- 	$(this).attr("src", g)
- })
+	$('.pop-content img').each(function() {
+		var g = $(this).attr("data-src")
+		$(this).attr("src", g)
+	})
 
 })
 
